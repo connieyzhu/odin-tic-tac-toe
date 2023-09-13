c3a2af2a563bd10f357d4d56cfcff0340e59a2a6
Connie <connieyzhu3@gmail.com>
2023-09-13 14:09:40 -0700

    Finalize game mechanics

--- script.js
+++ script.js
@@ -34,44 +34,84 @@ const displayController = (() => {
         e.target.style.backgroundColor = 'white';
     });
 
-    return {setMessage, getGrid};
+    const reset = () => {
+        for(i = 0; i < gameBoard().length; i++){
+            gameBoard()[i].textContent = '';
+        }
+    }
+
+    return {setMessage, getGrid, reset};
 })();
 
 const gameController = (() => {
     const playerX = Player('X');
     const playerO = Player('O');
     let round = 1;
+    let isOver = false;
 
     const getCurrentPlayerSign = () => {
         return round % 2 == 1 ? playerX.getSign() : playerO.getSign();
     }
 
     const playRound = () => {
-        round++;
-        checkWin();
-        displayController.setMessage(`Player ${getCurrentPlayerSign()}'s Turn`);
+        if(checkWin()){
+            displayController.setMessage(`Player ${getCurrentPlayerSign()} Wins!`)
+            isOver = true;
+        }else if(round == 9){
+            displayController.setMessage('Draw');
+        }else{
+            round++;
+            displayController.setMessage(`Player ${getCurrentPlayerSign()}'s Turn`);
+        }
     }
 
     const checkWin = () => {
+        const winPossibilities = [
+            [1, 2, 3],
+            [1, 4, 7],
+            [1, 5, 9],
+            [2, 5, 8],
+            [3, 5, 7],
+            [3, 6, 9],
+            [4, 5, 6],
+            [7, 8, 9]
+        ];
 
-    }
+        const checkSame = (arr) => {
+            const element = (index) => {
+                return gameBoard()[arr[index] - 1].textContent;
+            }
+
+            return (element(0) === element(1) && element(1) === element(2) &&
+                (element(0) === 'X' || element(0) === 'O'))
+        };
+
+        for(i = 0; i < winPossibilities.length; i++){
+            if(checkSame(winPossibilities[i])){
+                return true;
+            }
+        }
+
+        return false;
+    };
 
     displayController.getGrid().addEventListener('click', (e) => {
-        e.target.textContent = getCurrentPlayerSign();
-        playRound();
-        console.log(gameBoard());
+        if(e.target.textContent != '' || isOver){
+            return
+        }else{
+            e.target.textContent = getCurrentPlayerSign();
+            playRound();
+        }
     });
 
     const restartBtn = document.querySelector('.restart');
     restartBtn.addEventListener('click', (e) => {
-        //clear board, in displayController
-        //reset 1st player to x
+        displayController.reset();
+        isOver = false;
+        round = 1;
+        displayController.setMessage(`Player ${getCurrentPlayerSign()}'s Turn`);
     });
-
-    const dummy = () => console.log('hi');
-    return dummy;
+    
+    return playRound;
 
 })();
-
-
-gameController();
