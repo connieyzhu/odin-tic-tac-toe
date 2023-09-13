0759fc2a92a307205e8ccef19bc1963bb29b8cf8
Connie <connieyzhu3@gmail.com>
2023-09-12 21:42:35 -0700

    Set board array

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const Player = () => {
-    
+const Player = (sign) => {
+    return {sign};
 };
 
 const gameBoard = (() => {
-    const arr = [];
+    const board = Array.from(document.getElementsByClassName('square'));
+    const getBoard = () => console.log(board);
+    return getBoard;
 })();
 
 const displayController = (() => {
-
+    
 })();
 
 const gameController = (() => {
 
 })();
 
+gameBoard();
